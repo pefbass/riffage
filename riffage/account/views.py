96d8570bbfcba36765fdf4025de42824b281a4ac
pip <reevesrs24@gmail.com>
2017-10-18 22:12:21 -0700

    Added navbar

--- riffage/account/views.py
+++ riffage/account/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the accounts index.")
+     return render(request, 'index.html')
+
+def account(request):
+     return render(request, 'account.html')
