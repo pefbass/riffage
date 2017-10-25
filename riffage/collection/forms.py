bcbabd2a948f21f3240e9fe2ffadef2a135c2a3b
Preston Fraser <pefbass@gmail.com>
2017-10-24 19:58:00 -0700

    Fixed base.html margin-bottom

--- riffage/collection/forms.py
+++ riffage/collection/forms.py
@@ -4,4 +4,4 @@ from .models import Riff
 class RiffForm(ModelForm):
 	class Meta:
 		model = Riff
-		fields = ['name', 'riff_key', 'timesig_num', 'timesig_denom',]
+		fields = ['name', 'riff_key', 'timesig_num', 'timesig_denom', ]
