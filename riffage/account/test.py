6855f8d9cee778a2ca5ad42fd839fc3d6372798f
Morgan <gramorgan@gmail.com>
2017-12-03 16:54:13 -0800

    fix indentation in account test

--- riffage/account/test.py
+++ riffage/account/test.py
@@ -9,17 +9,19 @@ from django.contrib.auth.models import User
 # Create your tests here.
 
 class TestAccountApp(TestCase):
-    
-    def test_valid_form_create_page(self):
-        form = SignUpForm({
+	
+	def test_valid_form_create_page(self):
+		form = SignUpForm({
 			'username' : 'test_name',
-            'email' : 'test_email@gmail.com',
-            'password1' : 'password_1',
-            'password2' : 'password_1'
+			'email' : 'test_email@gmail.com',
+			'password1' : 'password_1',
+			'password2' : 'password_1'
 		})
-	self.assertTrue(form.is_valid())
+		
+		self.assertTrue(form.is_valid())
 
-    def test_valid_form_login_page(self):
-        self.user = User.objects.create_user(username='testuser', password='password_1')
-        login = self.client.login(username='testuser', password='password_1')
-	self.assertTrue(login)
+	def test_valid_form_login_page(self):
+		self.user = User.objects.create_user(username='testuser', password='password_1')
+		login = self.client.login(username='testuser', password='password_1')
+
+		self.assertTrue(login)
